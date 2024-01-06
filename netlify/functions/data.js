4ca274dbf0eeacd5a89ac86784f02c0d5d1d7593
tai03 <tdmius03@gmail.com>
2024-01-07 02:34:36 +0900

    ランダム

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -24,34 +24,6 @@ exports.handler = async () => {
      data.dog[i] = await fetchDagData();
      data.cat[i] = await fetchCatData();
   }
-  const data1 = {
-    "dog" : ["https://images.dog.ceo/breeds/dhole/n02115913_312.jpg",
-        "https://images.dog.ceo/breeds/pomeranian/n02112018_4840.jpg",
-        "https://images.dog.ceo/breeds/vizsla/n02100583_917.jpg",
-        "https://images.dog.ceo/breeds/terrier-border/n02093754_1111.jpg",
-        "https://images.dog.ceo/breeds/pekinese/n02086079_16268.jpg",
-        "https://images.dog.ceo/breeds/ridgeback-rhodesian/n02087394_909.jpg",
-        "https://images.dog.ceo/breeds/vizsla/n02100583_1214.jpg",
-        "https://images.dog.ceo/breeds/redbone/n02090379_3300.jpg",
-        "https://images.dog.ceo/breeds/eskimo/n02109961_3902.jpg",
-        "https://images.dog.ceo/breeds/ridgeback-rhodesian/n02087394_9511.jpg",
-        "https://images.dog.ceo/breeds/pyrenees/n02111500_8884.jpg",
-        "https://images.dog.ceo/breeds/labradoodle/Cali.jpg"
-        ],
-        "cat" : ["https://cdn2.thecatapi.com/images/bi0.jpg",
-        "https://cdn2.thecatapi.com/images/7kr.jpg",
-        "https://cdn2.thecatapi.com/images/94h.png",
-        "https://cdn2.thecatapi.com/images/snxE5_PPX.jpg",
-        "https://cdn2.thecatapi.com/images/2eh.jpg",
-        "https://cdn2.thecatapi.com/images/2g6.jpg",
-        "https://cdn2.thecatapi.com/images/7au.jpg",
-        "https://cdn2.thecatapi.com/images/gs.jpg",
-        "https://cdn2.thecatapi.com/images/MTc4MzM4MQ.jpg",
-        "https://cdn2.thecatapi.com/images/YnPrYEmfe.jpg",
-        "https://cdn2.thecatapi.com/images/b11.jpg",
-        "https://cdn2.thecatapi.com/images/MTUzNjQwNw.jpg"
-        ]
-  }
     return {
       statusCode: 200,
       headers: headers,
